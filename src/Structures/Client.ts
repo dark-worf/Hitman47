c85af49b0073c1761c95963f8024598cad22dd91
||•carter•|| <98878721+dark-worf@users.noreply.github.com>
2024-01-24 21:07:10 +0200

    Update Client.ts

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -30,7 +30,7 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
 
     public start = async (): Promise<client> => {
         await connect(
-            'mongodb+srv://herrison:wells@cluster0.tqhtuou.mongodb.net/?retryWrites=true&w=majority'
+            'mongodb+srv://hackedx:ping254@cluster0.qhbvyat.mongodb.net/?retryWrites=true&w=majority'
         )
         this.log('Connected to the Database')
         const { useDatabaseAuth } = new AuthenticationFromDatabase(this.config.session)
